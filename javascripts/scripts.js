aa78a9c575e22a030790c959c377b5d98da2b04f
fooenze <fooenze@gmail.com>
2015-03-09 18:26:06 +0800

    added personal map design

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
   var mapContainer = document.getElementById('map');
   mapContainer.style.height = window.innerHeight + 'px';
 
-  L.mapbox.accessToken = 'pk.eyJ1IjoianVhbmNnb256YSIsImEiOiJwTzBYblFBIn0.mrChbL1APmRc2iRak665kQ';
+  L.mapbox.accessToken = 'pk.eyJ1IjoiZm9vZW56ZSIsImEiOiJrMlpXc01JIn0.ymqjCa4-xulzMO3HYRxbvg';
 
   drawMap();
 });
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function drawMap () {
   var canvasTiles = L.tileLayer.canvas();
-  var map = L.mapbox.map('map', 'juancgonza.89abf372').setView([19.1562, 72.7631], 11);
+  var map = L.mapbox.map('map', 'fooenze.11a7a104').setView([19.1562, 72.7631], 11);
 
   L.canvasOverlay().drawing(drawPoints).addTo(map);
 }
@@ -18,10 +18,13 @@ function drawMap () {
 function drawPoints (canvasOverlay, params) {
   var ctx = params.canvas.getContext('2d');
   ctx.clearRect(0, 0, params.canvas.width, params.canvas.height);
+  
   ctx.beginPath();
   points.map(function (d, i) {
     dot = canvasOverlay._map.latLngToContainerPoint( [d[0], d[1]] );
     ctx.lineTo(dot.x, dot.y);
+
   });
-  ctx.stroke();
+            ctx.strokeStyle = '#ffffff';
+          ctx.stroke();
 }
